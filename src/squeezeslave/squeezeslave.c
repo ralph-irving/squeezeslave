a620a5cde69ec7163a877ad20cd42bcfb321fd3b
Ralph Irving <ralph_irving@hotmail.com>
2009-03-11 11:57:12 +0000

    Bring squeezeslave version in line with playertype changes.

--- src/squeezeslave/squeezeslave.c
+++ src/squeezeslave/squeezeslave.c
@@ -97,7 +97,7 @@ static void daemonize(char *);
 
 static volatile bool signal_exit_flag = false;
 static volatile bool signal_restart_flag = false;
-static const char* version = "0.8-29";
+static const char* version = "0.8-32";
 
 static int player_type = 8;
 
