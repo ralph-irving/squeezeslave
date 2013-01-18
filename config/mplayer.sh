9b6803a2607910580480dd2f850c095d11669585
Ralph Irving <ralph_irving@hotmail.com>
2013-01-18 19:14:03 +0000

    Capture working wma custom-convert.conf and mplayer.sh script. Tested with mplayer-1.0.rc3 which doesn't run at 100% cpu when streaming wma unlike 1.0rc4 and 1.1. Increased revision to 393.

--- config/mplayer.sh
+++ config/mplayer.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 scriptpid=$$
-apppid=/tmp/.alienbbc-app.$$.pid
+apppid=/tmp/.mplayer-app.$$.pid
 app=mplayer
 
 # For osx look for processor specific mplayer executable in locations used by mplayer installer
@@ -14,19 +14,26 @@ if [ `uname` = "Darwin" ] ; then
 	fi
 fi
 
-if [ -e "${10}" ] ; then
-    "$app" $1 $2 $3 $4 $5 $6 $7 $8 $9 "${10}" ${11} "${12}" "${13}" 3>&1 1>&2
+if [ -e "${12}" ] ; then
+    echo Local File: Launching $app "$@" >&2
+    "$app" "$@" 3>&1 1>&2
     exit
 fi
 
+echo Streaming: Launching $app "$@" >&2
 (
-    "$app" $* 3>&1 1>&2 & 
+    exec 3>&1
+    sleep 0.1
+    killall $app
+    "$app" "$@" 1>&2 & 
     echo $! > $apppid
     wait
+    killall $app
     rm -f $apppid
     kill $scriptpid 2> /dev/null
 ) < /dev/null &
 
+cat > /dev/null
 if [ -s $apppid ] ; then
     kill `cat $apppid` 2> /dev/null
 fi
